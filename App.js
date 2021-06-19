a5bc0e79e5190f94be34932dc46f07a0017e3120
FrankMbappe <kevyndrews@gmail.com>
2021-06-19 03:01:46 +0100

    Installed Mongoose

--- App.js
+++ App.js
@@ -1,23 +1,4 @@
-const http = require("http");
-const { VERSION } = require("lodash");
+const { VERSION, includes } = require("lodash");
 
 console.log(VERSION);
-
-const server = http.createServer((req, res) => {
-	if (req.url === "/") {
-		res.write("Hellow.. sir..");
-	}
-	if (req.url === "/api/courses") {
-		res.write(JSON.stringify([1, 2, 3]));
-	}
-	if (req.url === "/lareine") {
-		res.write(
-			"<a href='https://www.google.com/search?client=firefox-b-d&q=dineo+moeketsi'>Dineo Moeketsi</a>"
-		);
-	}
-	res.end();
-});
-
-server.listen(3000);
-
-console.log("Listening on port 3000...");
+console.log(includes([1, 2, 3], 3));
