fc4cc3fa4d3f6bf165eefccecce3c0d69a3322d7
FrankMbappe <kevyndrews@gmail.com>
2021-07-01 22:24:24 +0100

    Implemented the check phone number route

--- routes/check.js
+++ routes/check.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { User } = require("../models/User");
 const debug = require("debug")("ns:routes::check");
 const router = express.Router();
 
@@ -13,10 +14,31 @@ router.get("/:phone", async (req, res) => {
 	client.lookups.v1
 		.phoneNumbers(req.params.phone)
 		.fetch({ type: ["carrier"] })
-		.then((phone_number) => res.send(phone_number))
+		.then(async ({ countryCode, phoneNumber, nationalFormat }) => {
+			try {
+				const phoneNumberExists =
+					(await User.findOne({
+						phone: phoneNumber,
+					}).countDocuments()) > 0;
+
+				res.send({
+					countryCode,
+					phoneNumber,
+					nationalFormat,
+					phoneNumberExists,
+				});
+			} catch (error) {
+				debug(error);
+				return res
+					.status(500)
+					.send(
+						`An internal error occured while verifying if '${req.params.phone}' already exists.`
+					);
+			}
+		})
 		.catch((error) => {
 			debug(error);
-			res
+			return res
 				.status(404)
 				.send(`'${req.params.phone}' does not match any valid phone number.`);
 		});
