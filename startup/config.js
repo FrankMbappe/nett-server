ea4b442c3202e1d45354f210d2d193ad7afe1b88
FrankMbappe <kevyndrews@gmail.com>
2021-08-03 00:53:19 +0100

    Started using dotenv to manage environment variables

--- startup/config.js
+++ startup/config.js
@@ -1,17 +1,18 @@
+const dotenv = require("dotenv").config();
 const config = require("config");
 
 module.exports = function () {
-	if (
-		!config.get("jwtPrivateKey") ||
-		!config.get("twilioAccountSid") ||
-		!config.get("twilioAuthToken") ||
-		!config.get("twilioServiceId") ||
-		!config.get("cloudinaryCloudName") ||
-		!config.get("cloudinaryApiKey") ||
-		!config.get("cloudinaryApiSecret")
-	) {
-		throw new Error(
-			"FATAL ERROR: Some of the required the environment variables are not set."
-		);
-	}
+    if (
+        (!dotenv.error && !config.get("jwtPrivateKey")) ||
+        !config.get("twilioAccountSid") ||
+        !config.get("twilioAuthToken") ||
+        !config.get("twilioServiceId") ||
+        !config.get("cloudinaryCloudName") ||
+        !config.get("cloudinaryApiKey") ||
+        !config.get("cloudinaryApiSecret")
+    ) {
+        throw new Error(
+            "FATAL ERROR: Some of the required the environment variables are not set."
+        );
+    }
 };
